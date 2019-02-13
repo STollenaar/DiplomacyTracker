f42bc4b9309bed067e0ebeaf827677251669d666
Stephen Tollenaar <1guitargun1@gmaik.com>
2019-02-13 11:21:22 -0330

    able to scroll through the map time line

--- DiplomacyBot/bot.js
+++ DiplomacyBot/bot.js
@@ -1,4 +1,4 @@
-﻿var Discord = require('discord.js');
+﻿const { Client, RichEmbed } = require('discord.js');
 var auth = require('./auth.json');
 var request = require('request');
 var parser = require('cheerio-tableparser');
@@ -8,27 +8,17 @@ const cheerio = require('cheerio');
 var state = require('./state.json')[0];
 var site = "https://webdiplomacy.net/";
 
-var channelID;
-
-
-
-
+var channel;
 var siteContent;
-
-
+var mapIndex = 0;
 
 // Initialize Discord Bot
-var client = new Discord.Client();
+var client = new Client();
 
 client.on('ready', function (evt) {
     console.log("Connected");
 
-    for (var channel in client.channels) {
-        if (bot.channels[channel].name == "diplomacy") {
-            channelID = channel;
-            break;
-        }
-    }
+    channel = client.channels.find(ch => ch.name === "diplomacy");
 
     httpGet(function (response) {
         siteContent = response;
@@ -36,16 +26,16 @@ client.on('ready', function (evt) {
         const $ = cheerio.load(siteContent);
 
         //checking if the data is current
-        if (state.Date.replace("-", ", ") != $('span.gameDate').text()) {
+        if (state.Date.replace("-", ", ") !== $('span.gameDate').text()) {
             state.Date = $('span.gameDate').text().replace(", ", "-");
-            botSendMessage("Date is now " + state.Date.replace("-", ", "));
+            channel.send("Date is now " + state.Date.replace("-", ", "));
 
             parser($);
             var members = $('.membersFullTable').parsetable(false, false, true);
 
             for (var i = 0; i < members[0].length; i++) {
                 //some weird data is undefined
-                if (members[1][i * 2] == undefined) {
+                if (members[1][i * 2] === undefined) {
                     break;
                 }
                 //getting the player data
@@ -59,12 +49,12 @@ client.on('ready', function (evt) {
 
                 for (var p in state.Leaderboard) {
                     //updating player data
-                    if (p.name == name) {
+                    if (p.name === name) {
                         found = true;
-                        if (p.supply_centers != supply_centers) {
+                        if (p.supply_centers !== supply_centers) {
                             p.supply_centers = supply_centers;
                         }
-                        if (p.units != units) {
+                        if (p.units !== units) {
                             p.units = units;
                         }
                     }
@@ -91,75 +81,122 @@ client.on('ready', function (evt) {
     console.log("loading complete");
 });
 
-client.on('message', function (user, userID, channelID, message, evt) {
-    if (evt.d.mentions.length != 0 && evt.d.mentions[0].id == bot.id) {
+client.on('message', message => {
+    if (message.isMentioned(client.user.id)) {
 
-        var args = message.split(" ");
+        var args = message.content.split(" ");
         var cmd = args[1];
         args = args.slice(2, args.length - 1).join(" ");
-
+        const embed = new RichEmbed();
         switch (cmd) {
-           
+
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'pong'
-                });
+                channel.send('pong');
                 break;
+            case 'leaderboard':
             case 'standing':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Current standing as of '+ state.Date.replace("-", " "),
-                    embed: {
-                        "fields": leaderBoardbuilder()
-                    }
-                });
+                leaderBoardbuilder(embed);
+                channel.send(embed);
                 break;
             case 'map':
-                var src = site + "map.php?gameID=" + state.GameID + "&turn=" + getLatestMapIndex()
+                const filter = (reaction, user) => {
+                    return ['◀', '▶', '⏮','⏭'].includes(reaction.emoji.name) && user.id === message.author.id;
+                };
 
-                // console.log(src);
+                
 
-                bot.sendMessage({
-                    to: channelID,
-                    message: '',
-                    embed: {
-                        "image": {
-                            "url": src
+                embed.setImage(getMapSrc(-2));
+                embed.setTitle("Map as of " + state.Date.replace("-", " "));
+
+                //scrolling through map timeline
+                channel.send(embed).then(async embedMessage => {
+                    await embedMessage.react('⏮');
+                    await embedMessage.react('◀');
+                    await embedMessage.react('▶');
+                    await embedMessage.react('⏭');
+
+                    const collector = embedMessage.createReactionCollector(filter, { time: 180000 });
+
+                    collector.on('collect', (reaction, reactionCollector) => {
+                        const editEmbed = new RichEmbed();
+
+                        //scrolling correctly
+                        switch (reaction.emoji.name) {
+                            case '◀':
+                                if (mapIndex > -1) {
+                                    mapIndex--;
+                                } else {
+                                    return;
+                                }
+                                break;
+                            case '▶':
+                                if (mapIndex < getLatestMapIndex(-2)) {
+                                    mapIndex++;
+                                } else {
+                                    return;
+                                }
+                                break;
+                            case '⏭':
+                                mapIndex = getLatestMapIndex(-2);
+                                break;
+                            case '⏮':
+                                mapIndex = -1;
+                                break;
                         }
-                    }
+
+                        //completing edit
+                        editEmbed.setTitle(IndexToDate());
+                        editEmbed.setImage(getMapSrc(mapIndex));
+                        embedMessage.edit(editEmbed);
+                    });
                 });
                 break;
         }
     }
 });
 
-//gets a correct map index
-function getLatestMapIndex() {
+function getMapSrc(index) {
+    mapIndex = getLatestMapIndex(index);
+    return site + "map.php?gameID=" + state.GameID + "&turn=" + mapIndex;
+}
+
+function IndexToDate() {
+    var diff = Math.abs(mapIndex - getLatestMapIndex(-2));
+
     var season = state.Date.split("-")[0];
     var year = state.Date.split("-")[1];
 
-    return (year - state.startYear) * 2 + (season == state.startSeason ? 0 : 1);
-
-}
-
-function botSendMessage(m) {
-    client.sendMessage({
-        to: channelID,
-        message: m
-    });
-}
-
-function leaderBoardbuilder() {
-    var array = [];
-    for (var player in state.Leaderboard) {
-        elm = {
-            name: "Country: " + state.Leaderboard[player].country + ", Player by: " + state.Leaderboard[player].name,
-            value: "Supply-Centers: " + state.Leaderboard[player].supply_centers + ", Units: " + state.Leaderboard[player].units
+    //switching the season correctly
+    if (!(diff % 2 === 0)) {
+        if (season === "Spring") {
+            season = "Autum";
+        } else {
+            season = "Spring";
         }
-        array.push(elm);
     }
-    return array;
+    //setting the year correctly
+    year -= Math.ceil(diff / 2);
+    return season + " " + year;
+}
+
+//gets a correct map index
+function getLatestMapIndex(index) {
+    if (index !== -2) return index;
+    
+    var season = state.Date.split("-")[0];
+    var year = state.Date.split("-")[1];
+
+    return (year - state.startYear) * 2 + (season === state.startSeason ? 0 : 1)-1;//returning the correct map index
+}
+
+
+function leaderBoardbuilder(embed) {
+    for (var player in state.Leaderboard) {
+        embed.addField(
+            "Country: " + state.Leaderboard[player].country + ", Player by: " + state.Leaderboard[player].name,
+            "Supply-Centers: " + state.Leaderboard[player].supply_centers + ", Units: " + state.Leaderboard[player].units
+        );
+    }
 }
 
 
