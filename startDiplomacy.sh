083c98d735e9539cb3bed6909c3eda3e45319d0b
STollenaar <stephen@tollenaar.com>
2019-07-20 02:36:43 +0000

    detached mode for docker

--- startDiplomacy.sh
+++ startDiplomacy.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
 docker build --rm -t diplomacybot . 
 
-docker run --name diplomacybot --rm -t -i --log-driver=journald -v diplomacyBot:/var/ diplomacybot
+docker run -d --name diplomacybot --rm -t -i --log-driver=journald -v diplomacyBot:/var/ diplomacybot
 
